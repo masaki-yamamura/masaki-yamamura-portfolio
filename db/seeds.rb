ac7aacfc345ab17571cbd396f73c28bf23a187c9
masaki-yamamura <49702056+masaki-yamamura@users.noreply.github.com>
2019-11-10 22:15:35 +0900

    commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,157 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Post.create!(
+    [
+        {
+            text: "メモの魔力",
+            image: "https://images-na.ssl-images-amazon.com/images/I/510MVcp7zoL._SX338_BO1,204,203,200_.jpg",
+        },
+
+        {
+            text: "ゼロ秒思考",
+            image: "https://images-na.ssl-images-amazon.com/images/I/41mFGNSxZvL._SX343_BO1,204,203,200_.jpg",
+        },
+
+        {
+            text: "考具",
+            image: "https://images-na.ssl-images-amazon.com/images/I/41pojCP9XXL._SX350_BO1,204,203,200_.jpg",
+        },
+
+        {
+            text: "世界最強の商人",
+            image: "https://images-na.ssl-images-amazon.com/images/I/61VC842QM5L._SX350_BO1,204,203,200_.jpg",
+        },
+
+        {
+            text: "新しい文章力の教室",
+            image: "https://images-na.ssl-images-amazon.com/images/I/51VYH80kpuL._SX338_BO1,204,203,200_.jpg",
+        },
+
+        {
+            text: "HARD THINGS",
+            image: "https://images-fe.ssl-images-amazon.com/images/I/41NEFDfkNAL.jpg",
+        },
+
+        {
+            text: "ラ・ロシュフコー箴言集",
+            image: "https://images-na.ssl-images-amazon.com/images/I/51RJTK4CYGL.jpg",
+        },
+
+        {
+            text: "ハートドリブン",
+            image: "https://images-na.ssl-images-amazon.com/images/I/915GGTeNZXL.jpg",
+        },
+
+        {
+            text: "嫌われる勇気",
+            image: "https://images-fe.ssl-images-amazon.com/images/I/51pfi-lM-HL.jpg",
+        },
+
+        {
+            text: "幸せになる勇気",
+            image: "https://images-na.ssl-images-amazon.com/images/I/71VQbaPG4dL.jpg",
+        },
+
+        {
+            text: "自分の中に毒を持て",
+            image: "https://images-na.ssl-images-amazon.com/images/I/51FSEHVGFYL._SX331_BO1,204,203,200_.jpg",
+        },
+
+        {
+            text: "サーチ・インサイド・ユアセルフ",
+            image: "https://images-fe.ssl-images-amazon.com/images/I/41da4JhDohL.jpg",
+        },
+
+        {
+            text: "ゼロ・トゥ・ワン",
+            image: "https://images-na.ssl-images-amazon.com/images/I/71jHLe0jLtL.jpg",
+        },
+
+        {
+            text: "ハイパワー・マーケティング",
+            image: "https://images-na.ssl-images-amazon.com/images/I/51qrzxZ7DRL._SX338_BO1,204,203,200_.jpg",
+        },
+
+        {
+            text: "イシューからはじめよ",
+            image: "https://images-na.ssl-images-amazon.com/images/I/81yvg%2B4OJ8L.jpg",
+        },
+
+        {
+            text: "思考は現実化する",
+            image: "https://images-fe.ssl-images-amazon.com/images/I/51o-k4BbdxL.jpg",
+        },
+
+        {
+            text: "7つの習慣",
+            image: "https://images-na.ssl-images-amazon.com/images/I/71laXZxNTfL.jpg",
+        },
+
+        {
+            text: "できる課長は「これ」をやらない！",
+            image: "https://images-na.ssl-images-amazon.com/images/I/51%2B38eQ3FhL._SX345_BO1,204,203,200_.jpg",
+        },
+
+        {
+            text: "メンタルハック大全",
+            image: "https://images-na.ssl-images-amazon.com/images/I/51CY1vOFD5L._SX339_BO1,204,203,200_.jpg",
+        },
+
+        {
+            text: "限りなく黒に近いグレーな心理術",
+            image: "https://images-na.ssl-images-amazon.com/images/I/51krsvdUX6L._SX338_BO1,204,203,200_.jpg",
+        },
+
+        {
+            text: "伝え方が9割",
+            image: "https://images-na.ssl-images-amazon.com/images/I/51a55eJudnL._SX344_BO1,204,203,200_.jpg",
+        },
+
+        {
+            text: "入社一年目の教科書",
+            image: "https://images-na.ssl-images-amazon.com/images/I/41u1fcgn0ZL._SX339_BO1,204,203,200_.jpg",
+        },
+
+        {
+            text: "未来をつくる起業家",
+            image: "https://images-na.ssl-images-amazon.com/images/I/51ObsS8BjzL._SX349_BO1,204,203,200_.jpg",
+        },
+
+        {
+            text: "モチベーション革命",
+            image: "https://images-na.ssl-images-amazon.com/images/I/51a5stksl4L._SX338_BO1,204,203,200_.jpg",
+        },
+
+        {
+            text: "1440分の使い方",
+            image: "https://images-na.ssl-images-amazon.com/images/I/51O1yQV3MZL._SX338_BO1,204,203,200_.jpg",
+        },
+
+        {
+            text: "仕事は楽しいかね？",
+            image: "https://images-fe.ssl-images-amazon.com/images/I/51wO5nSiO9L.jpg",
+        },
+
+        {
+            text: "沈黙のwebマーケティング",
+            image: "https://images-na.ssl-images-amazon.com/images/I/51rYGe8cd2L._SX351_BO1,204,203,200_.jpg",
+        },
+
+        {
+            text: "沈黙のwebライティング",
+            image: "https://images-na.ssl-images-amazon.com/images/I/51TvUiUY%2BtL._SX350_BO1,204,203,200_.jpg",
+        },
+
+        {
+            text: "世界一わかりやすいマーケティングの本",
+            image: "https://images-fe.ssl-images-amazon.com/images/I/51jUH0Kly8L.jpg",
+        },
+
+        {
+            text: "人生の勝算",
+            image: "https://images-na.ssl-images-amazon.com/images/I/911%2BpeClX%2BL.jpg",
+        },
+    ]
+)
